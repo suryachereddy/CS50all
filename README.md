5432445a7c5f35dae01100b45998072b66a7c770
suryachereddy <suryachereddy@gmail.com>
2020-07-03 14:13:00 +0530

    initial

--- README.md
+++
@@ -1,3 +0,0 @@
-# Project 1
-
-This project is made for the Harvard C50's Web Programming with Python and JavaScript course from edx.org
--- SqlCommandUsed.sql
+++
@@ -1,9 +0,0 @@
---ONLY FOR REFERENCE
-
-
---login validate
-SELECT passhash FROM users WHERE username=:username
----isUserUnique
-SELECT * FROM users WHERE username=:username
---insert new user
-INSERT INTO users (username,passhash) values(:username,:passhash)
