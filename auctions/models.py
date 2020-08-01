738984a0692d1178d447f10e5f209478140a8c39
suryachereddy <suryachereddy@gmail.com>
2020-08-01 20:19:04 +0530

    final

--- auctions/models.py
+++ auctions/models.py
@@ -9,15 +9,28 @@ class Auction(models.Model):
     listedby=models.ForeignKey(User,on_delete=models.CASCADE,related_name="auctions")
     image=models.CharField(max_length=512,null=True)
     startingBid=models.FloatField()
-    currentBid=models.FloatField(default=100.00)
     category=models.CharField(max_length=64,default="other")
     title=models.CharField(max_length=64,default="Title")
     des=models.CharField(max_length=256,default="No Description.")
     date=models.DateTimeField(auto_now=True)
-    latestbid=models.IntegerField(null=True)
-
+    biddingclose=models.IntegerField(default=0)
+    currentbiduser=models.ForeignKey(User,on_delete=models.CASCADE,related_name="bid",null=True)
+    currentBid=models.FloatField(null=True)
+    def __str__(self):
+        return f"{self.title} is listed by {self.listedby}" 
 class Comments(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
     Auction=models.ForeignKey(Auction,on_delete=models.CASCADE,related_name="comments")
     comment=models.CharField(max_length=512)
+    def __str__(self):
+        return f"{self.user} commented {self.comment} on {self.Auction.title}" 
+class wishlist(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="wishlist")
+    auction=models.ForeignKey(Auction,on_delete=models.CASCADE)
 
+class bids(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE, related_name="bids")
+    auction=models.ForeignKey(Auction,on_delete=models.CASCADE,related_name="bids")
+    bid=models.FloatField(default=1.00)
+    def __str__(self):
+        return f"{self.user} bidded an amount of ${self.bid} on {self.auction}" 
--- auctions/urls.py
+++ auctions/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("createlist",views.createlist, name="createlist"),
     path("auctionpage/<int:title>",views.auctionpage, name="auctionpage"),
-    path("bid",views.bid,name="bid")
+    path("bid",views.bid,name="bid"),
+    path("category",views.category,name="category"),
+    path("category/<str:cat>",views.categoryl,name="categoryl"),
+    path('wishlist',views.Wishlist,name='wishlist'),
+    path('addwishlist/<int:auction>',views.addwishlist,name="addwishlist"),
+    path('closebid/<int:title>',views.closebid,name="closebid"),
+    path('removewish/<int:auction>',views.removewish,name="removewish")
 ]
